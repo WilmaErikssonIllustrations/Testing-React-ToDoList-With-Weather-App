f2f758742cc89e5840685f3285d80e6a33637a98
WilmaErikssonIllustrations <wilmaeriksson1@hotmail.com>
2026-09-14 12:31:51 +0200

    add testfolder and testfile structure for taskutils

---
+++ src/utils/tests/taskUtils.test.js
@@ -0,0 +1,16 @@
+import { describe, expect, test } from "vitest";
+import {
+  validateTitle,
+  filterTasks,
+  countActiveTasks,
+  countCompletedTasks,
+} from "../taskUtils";
+
+describe("taskUtils", () => {
+  describe("validateTitle", () => {
+    test("error if title is empty");
+    //arrange
+    //act
+    //assert
+  });
+});
