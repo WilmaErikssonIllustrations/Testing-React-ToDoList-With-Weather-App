5ae3ac4ef6737c113ada51bcdc6f0f7177592db9
WilmaErikssonIllustrations <wilmaeriksson1@hotmail.com>
2026-09-14 14:33:10 +0200

    fix: todoform name misspelling and add initial code structure to TodoForm testfile

---
+++ src/components/tests/TodoForm.test.jsx
@@ -0,0 +1,6 @@
+import { describe, expect, test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import TodoForm from "../TodoForm";
+
+describe("", () => {});
